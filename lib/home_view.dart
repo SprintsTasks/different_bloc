65a1fe6745da74cb76dbe7e6c9696097ab984e3c
modyelgen <modyelgen243@gmail.com>
2025-01-26 20:53:46 +0200

    update

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,4 +1,3 @@
-import 'package:different_blocs/constant.dart';
 import 'package:different_blocs/manager/counter/counter_bloc.dart';
 import 'package:different_blocs/manager/theme/theme_bloc.dart';
 import 'package:flutter/material.dart';
@@ -43,7 +42,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 BlocConsumer<CounterBloc, CounterStates>(
                     listener: (context, state) {
                   if (state is UpdateCounterNumberState) {
-                    if (state.updatedNumber == 10||state.updatedNumber==-10) {
+                    if (state.updatedNumber == 10) {
                       ScaffoldMessenger.of(context).showSnackBar(
                           SnackBar(content: Text("${state.updatedNumber}")));
                     }
@@ -62,29 +61,14 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           floatingActionButton: BlocBuilder<CounterBloc, CounterStates>(
               builder: (context, state) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              spacing: 12,
-              children: [
-                FloatingActionButton(
-                  heroTag: 'Fat1',
-                  onPressed: () {
-                    context.read<CounterBloc>().changeCounter(_counter,
-                        pickedOperation: PickedOperation.add);
-                  },
-                  tooltip: 'Increment',
-                  child: const Icon(Icons.add),
-                ),
-                FloatingActionButton(
-                  heroTag: 'Fat2',
-                  onPressed: () {
-                    context.read<CounterBloc>().changeCounter(_counter,
-                        pickedOperation: PickedOperation.subtract);
-                  },
-                  tooltip: 'Decreament',
-                  child: const Icon(Icons.minimize_outlined),
-                ),
-              ],
+            return FloatingActionButton(
+              onPressed: () {
+                context
+                    .read<CounterBloc>()
+                    .add(ChangeCounterEvent(changedNumber: _counter));
+              },
+              tooltip: 'Increment',
+              child: const Icon(Icons.add),
             );
           }),
         ));
--- lib/manager/counter/counter_bloc.dart
+++ lib/manager/counter/counter_bloc.dart
@@ -1,19 +1,15 @@
 import 'package:bloc/bloc.dart';
-import 'package:different_blocs/constant.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 part 'counter_states.dart';
+part 'counter_event.dart';
 
-class CounterBloc extends Cubit<CounterStates> {
-  CounterBloc() : super(InitialCounterState());
+class CounterBloc extends Bloc<CounterEvent, CounterStates> {
+  CounterBloc() : super(InitialCounterState()) {
+    on<ChangeCounterEvent>(_changeCounter);
+  }
 
-  Future<void> changeCounter(int number,
-      {required PickedOperation pickedOperation}) async {
-    switch (pickedOperation) {
-      case PickedOperation.add:
-        emit(UpdateCounterNumberState(updatedNumber: ++number));
-
-      case PickedOperation.subtract:
-        emit(UpdateCounterNumberState(updatedNumber: --number));
-    }
+  Future<void> _changeCounter(
+      ChangeCounterEvent event, Emitter<CounterStates> emit) async {
+    emit(UpdateCounterNumberState(updatedNumber: event.changedNumber + 1));
   }
 }
